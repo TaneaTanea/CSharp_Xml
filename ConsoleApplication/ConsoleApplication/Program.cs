c2dd017708b3026f42935d5a061794cebe2caf21
campus <campus@S31911>
2018-10-27 11:45:54 +0300

    Read xml file example

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
 
 namespace ConsoleApplication
 {
@@ -11,6 +14,42 @@ namespace ConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("XML");
+            //CreateXmlFile();
+            ReadXML();
         }
+        public static void CreateXmlFile()
+        {
+            var listUsers = new List<User>()
+            {
+                new User { Name = "John Rambo", Age = 25, Company= "Holiwood"},
+                new User { Name = "John Smith", Age = 35, Company= "LuterCorp"},
+                new User { Name = "Jenifer lopez", Age = 22, Company= "Muzic"},
+            };
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<User>));
+            TextWriter Filestream = new StreamWriter(@"D:\User.xml");
+            serialiser.Serialize(Filestream, listUsers);
+            Filestream.Close();
+
+        }
+        public static void ReadXML()
+        {
+            var xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            int i = 0;
+            string str = null;
+            FileStream fs = new FileStream(@"D:\User.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("User");
+            for (i = 0; i <= xmlnode.Count - 1; i++)
+            {
+                xmlnode[i].ChildNodes.Item(0).InnerText.Trim();
+                str = xmlnode[i].ChildNodes.Item(0).InnerText.Trim() + " " + xmlnode[i].ChildNodes.Item(1).InnerText.Trim() + " " + xmlnode[i].ChildNodes.Item(2).InnerText.Trim();
+                Console.WriteLine(str);
+            }
+            fs.Close();
+            Console.WriteLine("\n End of file");
+            Console.ReadKey();
+        }
+
     }
 }
